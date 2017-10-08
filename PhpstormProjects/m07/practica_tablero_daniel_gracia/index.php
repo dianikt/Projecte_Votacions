9c2403849f05fe4919c3a628694da64913384f19
danigracia <gracia.danii@gmail.com>
2017-10-08 19:06:13 +0200

    subido

---
+++ PhpstormProjects/m07/practica_tablero_daniel_gracia/index.php
@@ -0,0 +1,17 @@
+<html>
+
+    <head>
+    </head>
+
+    <body>
+        <h1>Tablero</h1>
+        <form method="post" action="resultat.php">
+            Filas:<br>
+            <input type="number" name="filas" min="1" max="10"/><br><br>
+            Columnas:<br>
+            <input type="number" name="col" min="1" max="10"/><br><br>
+            <input type="submit" value="Enviar"/>
+        </form>
+
+    </body>
+</html>
