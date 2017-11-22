ef39d498a47c8e8a801c2647cadd77aeab7deb32
danigracia <gracia.danii@gmail.com>
2017-11-22 17:42:43 +0100

    dd

--- creacioConsultes.php
+++ creacioConsultes.php
@@ -8,26 +8,11 @@
  <head>
 	 <meta charset="utf-8">
     <title>Creación de preguntas</title>
-     <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">-->
-     <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/css/bootstrap-datetimepicker.min.css" rel="stylesheet">
-     <link href="css/style.css" rel="stylesheet" type="text/css">
-    <script href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/js/bootstrap-datetimepicker.min.js"></script>
-
+     <script src="js/javascript.js"></script>
     <script type="text/javascript">
         $("#datetime").datetimepicker({ format: 'yyyy-mm-dd hh:ii'});
     </script>
-	  	<title>Creación de preguntas</title>
-	  	  <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-		  <link rel="stylesheet" href="/resources/demos/style.css">
-		  <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-		  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-		  <script src="javascript.js"></script>
-		  <script>
-			  $( function() {
-			    $( ".datepicker" ).datepicker();
-			  } );
- 		 </script>
-	  	 	
+     
  </head> 
 	 <body>
 
