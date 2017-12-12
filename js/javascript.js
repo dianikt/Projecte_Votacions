e9aa6b4653c00ebd52512a3a30d636946798f4d9
danigracia <gracia.danii@gmail.com>
2017-12-12 16:06:11 +0100

    sdad

--- js/javascript.js
+++ js/javascript.js
@@ -2,6 +2,23 @@ var numconsulta = 0;
 var comprueba = 0;
 var numRespuestas = 0;
 var contador = 0;
+var today = obtenerFechaActual();
+function obtenerFechaActual(){
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1; //January is 0!
+    var yyyy = today.getFullYear();
+    if(dd<10) {
+        dd = '0'+dd
+    }
+
+    if(mm<10) {
+        mm = '0'+mm
+    }
+
+    today = yyyy + '-' + mm + '-' + dd;
+    return today;
+}
  //document.getElementById("demo").innerHTML = "You wrote: " + valor;
 
  // Funcion que añade una pregunta en un tag<li> a una lista <ul>
@@ -9,15 +26,13 @@ var contador = 0;
 {
 	
 	var nuevo = document.getElementById("crear_respuesta").value;
-
-    if(nuevo.length>0) // si esta vacio no creara otro input 
+    if(nuevo.length>0) // si esta vacio no creara otro input
     {
         if(buscarElementoLi(nuevo))
         {
         	contador++;        	
             var li = document.createElement('li');  // creamos el elemento <li>
-            li.setAttribute('id', nuevo);
-            li.setAttribute('value', nuevo);
+            li.setAttribute('id', contador);
 
             var input = document.createElement('input');
             var textPregunta = document.createTextNode(nuevo);
@@ -30,19 +45,44 @@ var contador = 0;
 			var boton = document.createElement('input');	
 		    boton.setAttribute('type', 'button');  //creamos el boton para eliminar
 			boton.setAttribute('value', 'Eliminar');
-			boton.setAttribute('onclick', 'eliminarRespuesta(this)');
-			li.appendChild(input);     //añadimos al <li> los elementos creados 
-			li.appendChild(boton);  
-            document.getElementById("listaDesordenada").appendChild(li); // y luego a la lista el <li>        	
-        	
+            boton.setAttribute('onclick', 'eliminarRespuesta(this)');
+
+            var botonSubir = document.createElement('input');
+            botonSubir.setAttribute('type', 'button');  //creamos el boton para subir la respuesta
+            botonSubir.setAttribute('id', 'subirRespuesta'+contador);
+            botonSubir.setAttribute('value', 'Subir');
+            botonSubir.setAttribute('onclick', 'subirRespuesta('+contador+')');
+
+            var botonBajar = document.createElement('input');
+            botonBajar.setAttribute('type', 'button');  //creamos el boton para bajar la respuesta
+            botonBajar.setAttribute('id', 'bajarRespuesta'+contador);
+            botonBajar.setAttribute('value', 'Bajar');
+            botonBajar.setAttribute('onclick', 'bajarRespuesta('+contador+')');
+
+
+            li.appendChild(input);     //añadimos al <li> los elementos creados
+            li.appendChild(boton);
+            li.appendChild(botonSubir);
+            li.appendChild(botonBajar);
+
+            document.getElementById("listaDesordenada").appendChild(li); // y luego a la lista el <li>
+
+
             var audio = carga_sonido(1); // cargamos el sonido de giro 
             audio.play();
 
             document.getElementById("crear_respuesta").value = ' ';
         	numRespuestas++;
+            if(numRespuestas==2){
+                crearBotonEnviarDatos();
+            }
+            correcto("Respuesta creada correctamente!")
         }
     }
-    if (numRespuestas == 2)crearBotonEnviarDatos();
+    else{
+        error("No puedes crear una respuesta vacia!")
+
+    }
     return false;
 }
 
@@ -57,6 +97,63 @@ function buscarElementoLi(contenido)
     }
     return true;
 }
+function bajarRespuesta(elemento) {
+    var linea=document.getElementById(elemento);
+    var cloneLinea = linea.cloneNode(true);
+    var padre = document.getElementById("listaDesordenada");
+    if(elemento==(padre.length-1)){
+        error("Es el ultimo de la lista, no se puede bajar!");
+    }
+    else{
+        padre.insertBefore(cloneLinea, padre.childNodes[elemento+1]);
+        padre.removeChild(padre.childNodes[elemento-1]);
+        document.getElementById('subirRespuesta'+elemento).setAttribute('onclick','subirRespuesta('+(elemento+1)+')');
+        document.getElementById('bajarRespuesta'+elemento).setAttribute('onclick','bajarRespuesta('+(elemento+1)+')');
+
+
+        document.getElementById('subirRespuesta'+(elemento+1)).setAttribute('onclick','subirRespuesta('+(elemento)+')');
+        document.getElementById('bajarRespuesta'+(elemento+1)).setAttribute('onclick','bajarRespuesta('+(elemento)+')');
+
+        document.getElementById('subirRespuesta'+elemento).setAttribute('id','subirRespuesta'+(elemento+1));
+        document.getElementById('bajarRespuesta'+elemento).setAttribute('id','bajarRespuesta'+(elemento+1));
+
+
+        document.getElementById('subirRespuesta'+(elemento+1)).setAttribute('id','subirRespuesta'+(elemento));
+        document.getElementById('bajarRespuesta'+(elemento+1)).setAttribute('id','bajarRespuesta'+(elemento));
+        document.getElementById(''+elemento+1).setAttribute('id',''+elemento);
+        document.getElementById(''+elemento).setAttribute('id',''+elemento+1);
+
+
+    }
+
+
+}
+function subirRespuesta(elemento) {
+    var linea=document.getElementById(elemento);
+    var cloneLinea = linea.cloneNode(true);
+    var padre = document.getElementById("listaDesordenada");
+    if(elemento==1){
+        error("Es el primero de la lista, no se puede subir!");
+    }
+    else{
+        cloneLinea.getElementById('subirRespuesta'+elemento).setAttribute('onclick','subirRespuesta('+(elemento-1)+')');
+        cloneLinea.getElementById('bajarRespuesta'+elemento).setAttribute('onclick','bajarRespuesta('+(elemento-1)+')');
+        cloneLinea.getElementById('subirRespuesta'+elemento).setAttribute('id','subirRespuesta'+(elemento-1));
+        cloneLinea.getElementById('bajarRespuesta'+elemento).setAttribute('id','bajarRespuesta'+(elemento-1));
+        padre.insertBefore(cloneLinea, padre.childNodes[element-1]);
+        padre.removeChild(padre.childNodes[elemento-1]);
+    }
+}
+
+function cambiar(obj,num1,num2) {
+    proVal = obj.options[num1].value;
+    proTex = obj.options[num1].text;
+    obj.options[num1].value = obj.options[num2].value;
+    obj.options[num1].text = obj.options[num2].text;
+    obj.options[num2].value = proVal;
+    obj.options[num2].text = proTex;
+    obj.selectedIndex = num2;
+}
 
 // Funcion para eliminar los elementos
 function eliminarRespuesta(elemento)  // recibe el elemento pulsado
@@ -64,18 +161,24 @@ function eliminarRespuesta(elemento)  // recibe el elemento pulsado
     var id = elemento.parentNode.getAttribute("id");
     idElemento = document.getElementById(id);
     idElemento.parentNode.removeChild(idElemento);
+    numRespuestas--;
+    if(numRespuestas<2){
+        borrarEnviarDatos();
+    }
+    correcto("Respuesta eliminada correctamente!")
 }
 
 function restarFechas() { //comprueba que la fecha de inicio no sea mayor que la final 
 	var fech1 = document.getElementById('fecha_inicio').value;
 	var fech2 = document.getElementById('fecha_final').value;
 
-    if((Date.parse(fech1)) == (Date.parse(fech2))){
+    if((Date.parse(today)) > (Date.parse(fech1))){
         return 1;
     }
-	if((Date.parse(fech1)) > (Date.parse(fech2))){
-		return 2;
-	}else return 0;
+	else if((Date.parse(fech1)) <= (Date.parse(fech2))) {
+        return 2;
+    }
+    else return 0;
 }
 
 // crea el input de respuesta
@@ -85,7 +188,7 @@ function crearInputRespuesta(){
     var input = document.createElement("input");
     input.setAttribute('type', 'text');
     input.setAttribute('id', 'crear_respuesta');   
-  	padre.parentNode.insertBefore(input, padre.nextSibling);
+  	padre.insertBefore(input, padre.childNodes[0]);
 
 } 
 
@@ -97,12 +200,40 @@ function pintarRojo(elemento){
 function borrarRojo(elemento){ // quita el border rojo
 	elemento.style.boxShadow='';
 }
+function borrarEnviarDatos(){
+    var botonEnviar = document.getElementById("enviarPreguntas");
+    while (botonEnviar.firstChild) {
+        botonEnviar.removeChild(botonEnviar.firstChild);
+    }
+}
+function borrarRespuestas(){
+    var listaRes = document.getElementById("listaDesordenada");
+    if(listaRes.length==0){
+       error("No puedes hacer eso, no hay respuestas a eliminar!");
+    }
+    else {
+        while (listaRes.firstChild) {
+            listaRes.removeChild(listaRes.firstChild);
+        }
+        borrarEnviarDatos();
+        numRespuestas = 0;
+        correcto("Respuestas eliminadas correctamente!")
+    }
+}
 
-function editar(elemento){ 
-	//alert("entro");
-    var elementoConsulta = document.getElementById(elemento);
-    elementoConsulta.disabled = false;
-    comprueba--;
+function editar(elemento){
+    if(elemento=="consulta"){
+        var edit = document.getElementById("icon0");
+        edit.disabled = false;
+    }
+    else if(elemento=="fecha_inicio"){
+        var edit = document.getElementById("icon1");
+        edit.disabled = false;
+    }
+    else if(elemento=="fecha_final"){
+        var edit = document.getElementById("icon2");
+        edit.disabled = false;
+    }
 }
 
 
@@ -112,14 +243,16 @@ function crearConsulta(){             // crea la consulta cuando le das al boton
     var br = document.createElement("br");
     var icon = document.createElement("img");
     padre.parentNode.insertBefore(br, padre.nextSibling);
+    icon.setAttribute('id', 'icon0');
     icon.setAttribute('class', 'edit-icon');
     icon.setAttribute('src', 'img/icon-edit.png');
+    icon.disabled = true;
     icon.setAttribute('onclick', 'editar("consulta")');
     padre.parentNode.insertBefore(icon, padre.nextSibling);
     input.setAttribute('type', 'text');
     input.setAttribute('name', 'consulta_enviar');
     input.setAttribute('id', 'consulta');     
-    input.setAttribute('onfocusout', 'validarCampoVacio("consulta")');    
+    input.setAttribute('onfocusout', 'validarCampoVacio()');
     padre.parentNode.insertBefore(input, padre.nextSibling);
 
 	var label = document.createElement("label");
@@ -127,6 +260,15 @@ function crearConsulta(){             // crea la consulta cuando le das al boton
     label.appendChild(insertarTexto);
     padre.parentNode.insertBefore(label, padre.nextSibling);
     
+}
+
+function crearBotonEliminarRespuestas() {
+    var padre = document.getElementById("crearRespuesta");
+    var boton = document.createElement("input");
+    boton.setAttribute('type', 'button');
+    boton.setAttribute('value', 'Borrar respuestas');
+    boton.setAttribute('onclick', 'borrarRespuestas()');
+    padre.insertBefore(boton, padre.childNodes[0]);
 }
 
 
@@ -137,14 +279,16 @@ function crearFechaFinal(){         // crea los input de las fechas cuando le da
     var inputFinal = document.createElement("input");
     var icon = document.createElement("img");
     padre.parentNode.insertBefore(br, padre.nextSibling);
+    icon.setAttribute('id', 'icon2');
     icon.setAttribute('class', 'edit-icon');
     icon.setAttribute('src', 'img/icon-edit.png');
+    icon.disabled = true;
     icon.setAttribute('onclick', 'editar("fecha_final")');
     padre.parentNode.insertBefore(icon, padre.nextSibling);
     inputFinal.setAttribute('type', 'date');
     inputFinal.setAttribute('name', 'fecha_final');
     inputFinal.setAttribute('id', 'fecha_final'); 
-    inputFinal.setAttribute('onfocusout', 'validarCampoVacio("fecha_final")'); 
+    inputFinal.setAttribute('onfocusout', 'validarFechaFin()');
     padre.parentNode.insertBefore(inputFinal, padre.nextSibling);	
 
 	var labelFinal = document.createElement("label");
@@ -160,14 +304,16 @@ function crearFechaInicio(){  // crea los input de las fechas cuando le das al b
 	var br = document.createElement("br");
     var inputInicio = document.createElement("input");
     var icon = document.createElement("img");
+    icon.setAttribute('id', 'icon1');
     icon.setAttribute('class', 'edit-icon');
     icon.setAttribute('src', 'img/icon-edit.png');
+    icon.disabled = true;
     icon.setAttribute('onclick', 'editar("fecha_inicio")');
     padre.parentNode.insertBefore(icon, padre.nextSibling);
     inputInicio.setAttribute('type', 'date');
     inputInicio.setAttribute('name', 'fecha_inicio');
     inputInicio.setAttribute('id', 'fecha_inicio');     
-    inputInicio.setAttribute('onfocusout', 'validarCampoVacio("fecha_inicio")'); 
+    inputInicio.setAttribute('onfocusout', 'validarFechaIni()');
   	padre.parentNode.insertBefore(inputInicio, padre.nextSibling);
 
 	var labelInicio = document.createElement("label");
@@ -176,44 +322,81 @@ function crearFechaInicio(){  // crea los input de las fechas cuando le das al b
     padre.parentNode.insertBefore(labelInicio, padre.nextSibling); 	
 	
 }
+function validarCampos() {
+    if(comprueba==3){
+        crearBotonRespuestas();
+    }
+}
+
+function validarFechaIni(){
+    var fechaIni = document.getElementById("fecha_inicio").value;
+    var elementoConsulta = document.getElementById("fecha_inicio");
+    var edit = document.getElementById("icon1");//Icono de habilitar editar campo
+    if(today >= fechaIni){
+        error('La fecha inicial no puede ser inferior o igual a la fecha actual!');
+        pintarRojo(elementoConsulta);
+    }
+    else{
+        borrarRojo(elementoConsulta);
+        elementoConsulta.disabled = true;
+        edit.disabled = false;
+        correcto("Correcto!");
+        comprueba++;
+        validarCampos();
+    }
+}
+function validarFechaFin(){
+    var fechaFin = document.getElementById("fecha_final").value;
+    var fechaIni = document.getElementById("fecha_inicio").value;
+    var edit = document.getElementById("icon2");
+    var elementoConsulta = document.getElementById("fecha_final");
+    if(fechaFin <= fechaIni){
+        error('La fecha final tiene que ser mayor a la fecha inicial!');
+        pintarRojo(elementoConsulta);
+    }
+    else{
+        borrarRojo(elementoConsulta);
+        elementoConsulta.disabled = true;
+        edit.disabled = false;
+        correcto("Correcto!");
+        comprueba++;
+        validarCampos();
+    }
+}
 
 
-function validarCampoVacio(elemento){ // comprueba los campos vacios
-	var consulta = document.getElementById(elemento).value;
-	var elementoConsulta = document.getElementById(elemento);
-
-	if (consulta == '') pintarRojo(elementoConsulta);
+function validarCampoVacio(){
+	var consulta = document.getElementById("consulta").value;
+	var elementoConsulta = document.getElementById("consulta");
+    var edit = document.getElementById("icon0");
+    if (consulta == '') {
+        error('El campo no puede estar vacio!');
+        pintarRojo(elementoConsulta);
+    }
 
 	else {
-        //elementoConsulta.setAttribute('disabled', 'true');
         elementoConsulta.setAttribute('value',consulta);
-		borrarRojo(elementoConsulta);
-		correcto("Correcto!");
-		comprueba++;
-	}	
-	
-	if (comprueba>=3){ 
-		var fechas = restarFechas();
-		if (fechas==2)alert('La fecha inicial no puede ser mayor que la fecha final');
-        else if (fechas==1)alert('La fecha inicial no puede ser igual que la fecha final'); 		
-		else{
-			crearBotonRespuestas();	
-			}	
-	}
+        borrarRojo(elementoConsulta);
+        elementoConsulta.disabled = true;
+        edit.disabled = false;
+        correcto("Correcto!");
+        comprueba++;
+        validarCampos();
+    }
 }
 
 
 function crearBotonRespuestas(){ // crea el boton para añadir respuestas
-	var padre = document.getElementById("crearRespuesta");
-	var br = document.createElement("br");	
+    crearBotonEliminarRespuestas();
+    var padre = document.getElementById("crearRespuesta");
+    var br = document.createElement("br");
     var boton = document.createElement("input");
     boton.setAttribute('type', 'submit');
     boton.setAttribute('value', 'Crear respuesta');
     boton.setAttribute('onclick', 'return nuevaRespuesta()');
-	padre.parentNode.insertBefore(boton, padre.nextSibling);
-	padre.parentNode.insertBefore(br, padre.nextSibling);
-	crearInputRespuesta();	
-
+	padre.insertBefore(boton, padre.childNodes[0]);
+    padre.insertBefore(br, padre.childNodes[0]);
+    crearInputRespuesta();
 }
 
 function crearBotonEnviarDatos(){  // boton para enviar los datos al servidor
@@ -221,10 +404,9 @@ function crearBotonEnviarDatos(){  // boton para enviar los datos al servidor
 	var br = document.createElement("br");	
     var boton = document.createElement("input");
     boton.setAttribute('type', 'submit');
-    boton.setAttribute('value', 'Enviar preguntas');
-	padre.parentNode.insertBefore(boton, padre.nextSibling);
-	padre.parentNode.insertBefore(br, padre.nextSibling);
-
+    boton.setAttribute('value', 'Enviar pregunta');
+	padre.insertBefore(boton, padre.childNodes[0]);
+	padre.insertBefore(br, padre.childNodes[0]);
 }
 
 function Votaciones()
